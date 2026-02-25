5378e6dae1e487499ff39d0793c4e2c981ada5ef
riaduhb <1501303212@qq.com>
2026-02-25 20:14:49 +0800

    重构前端架构，添加基础布局和API

---
+++ du-ai-code-mother-frontend/openapi2ts.config.ts
@@ -0,0 +1,6 @@
+// 根据 OpenAPI 文档生成 TypeScript 类型和服务端代码
+export default {
+  requestLibPath: "import request from '@/request'",
+  schemaPath: 'http://localhost:8123/api/v3/api-docs',
+  serversPath: './src',
+}
